6b1baa3a114a247ecd0f314b9aca5996b02b61ba
Antonio Andrew <antonioandrew92@gmail.com>
2019-09-29 15:40:48 -0400

    Adding iOS project to existing repo to test xCode Git functionalities

---
+++ ARKitBasics1/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  ARKitBasics1
+//
+//  Created by Antonio Andrew on 9/18/19.
+//  Copyright © 2019 Antonio Andrew. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        let result = sceneView.hitTest(touch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
+        guard let hitResult = result.last else {return}
+        let hitTransform = SCNMatrix4.init(hitResult.worldTransform)
+        let hitVector = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+        createBall(position: hitVector)
+        
+    }
+    
+    func createBall(position : SCNVector3){
+        let modelScene = SCNScene(named: "art.scnassets/mm_small.obj")
+        guard let modelNode = modelScene?.rootNode else {print("Returning"); return}
+        modelNode.position = position
+        sceneView.scene.rootNode.addChildNode(modelNode)
+        
+       /* var ballShape = SCNSphere(radius: 0.01)
+        var ballNode = SCNNode(geometry: ballShape)
+        ballNode.position = position
+        sceneView.scene.rootNode.addChildNode(ballNode)*/
+        
+        /*guard let tempScene = SCNScene(named: "art.scnassets/mm_small.obj") else
+        {
+            print("Object not found")
+            return
+        }
+        let modelNode = tempScene.rootNode
+        modelNode.simdPosition = SIMD3(0, 0, 0.5)
+        sceneView.scene.rootNode.addChildNode(modelNode)*/
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
